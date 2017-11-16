747f4725722f29dc88144349962c8eb9f2522ecd
Federico Calarco <validatebryu@iMacdiFederico.station>
2017-11-16 14:15:56 +0100

    commit for David

--- client/renderings/index.jsx
+++ client/renderings/index.jsx
@@ -17,16 +17,65 @@ class Index extends React.Component{
 
 
 
-      <Header/>
-      <Separator text="Recent News"/>
-      <Crowdsale/>
-      <Separator text="How it works"/>
+        <Header/>
+
+          <Separator text="Recent News"/>
+
+            <Crowdsale/>
+
+              <Separator text="How it works"/>
+
+        {/*         <Footer/>           */}
 
 
+          <section className="col-12" id="footer">
+
+            <div className="row no-gutters d-flex">
+
+                <h3 className="col-12 text-center">
+                  Contact Us
+                </h3>
+
+                <p className="col-12 text-center">contact@nova.mining</p>
+
+                <label  className="col-6 mx-auto">
+                <p className="col-12 text-center">Subscribe to the nova newsletter</p>
+
+                  <span className="row no-gutters">
+                    <input type="text" placeholder="Your email address" className="col-8 text-center"/>
+                    <button className="col-4">Subscribe</button>
+                  </span>
+                </label>
 
 
+                <div className="col-9 mx-auto">
+                  <div className="row no-gutters">
+                  <div className="col-4">
+                    <div className="row no-gutters">
+                      <h4 className="col-12 text-center">Community</h4>
+                    </div>
+                  </div>
 
-</div>
+                    <div className="col-4">
+                      <div className="row no-gutters">
+                        <h4 className="col-12 text-center">Follow</h4>
+                      </div>
+                    </div>
+
+                    <div className="col-4">
+                      <div className="row no-gutters">
+                        <h4 className="col-12 text-center">Documents</h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+            </div>
+
+          </section>
+
+
+      </div>
 
 
     )
