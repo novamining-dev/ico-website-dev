ab83be8619a87d1262d5553e8f7c993c2a50bc15
CalarDotCo <f3.calarco@gmail.com>
2018-06-30 01:49:50 +0200

    animation_layout

--- client/renderings/documents.jsx
+++ client/renderings/documents.jsx
@@ -14,7 +14,7 @@ export const Documentation = ({match}) => {
                 <p className="green col-12 text-center upcase p  mb-50" >Overview on</p>
                 <h1 className="col-12 text-center upcase mb-50">Documents & <strong>Media</strong></h1>
 
-                <div className="row no-gutters d-flex justify-content-center">
+                <div className="row no-gutters d-flex justify-content-center animated fadeInDown">
                 <button className="radius col-auto col-sm-4 col-md-2  bgreen white  bordergreen light f15 upcase vhb" style={{padding:10 , margin:15 , minWidth:170 }}><i className="fab fa-file-pdf fa " style={{marginRight:5}}></i>Get Whitepaper</button>
                 <button className="radius col-auto col-sm-4 col-md-2  bgreen white  bordergreen light f15 upcase vhb" style={{padding:10 , margin:15 , minWidth:170 }}><i className="fab fa-file-pdf fa " style={{marginRight:5}}></i>Get Whitepaper</button>
                 <button className="radius col-auto col-sm-4 col-md-2  bgreen white  bordergreen light f15 upcase vhb" style={{padding:10 , margin:15 , minWidth:170 }}><i className="fab fa-file-pdf fa " style={{marginRight:5}}></i>Get Whitepaper</button>
@@ -48,8 +48,8 @@ export const Documentation = ({match}) => {
                                 let date = text.slice(0 , [point + 4])
 
                                 return(
-                                  <div className="col-12 col-sm-4 mb-50 " key={article.id} onClick={()=> window.open(article.url)}>
-                                    <div className="row no-gutters d-flex justify-content-center text-left ">
+                                  <div className="col-12 col-sm-4 mb-50  " key={article.id} onClick={()=> window.open(article.url)}>
+                                    <div className="row no-gutters d-flex animated fadeInUp justify-content-center text-left ">
                                         <img src={article.url_image} className=" hoverable col-10 h-75 mb-15"  alt=""/>
                                         <p className="col-10 upcase  hoverable green f15">{date}</p>
                                         <h3 className="col-10  limited hoverable ">{article.title}</h3>
